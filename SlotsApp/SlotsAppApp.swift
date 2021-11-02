353e18504c09d702d0cf665e2d6a20f8a827c62c
Nsi4b <Nsi4b@yahoo.com>
2021-11-02 03:11:17 +0300

    first

---
+++ SlotsApp/SlotsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsAppApp.swift
+//  SlotsApp
+//
+//  Created by Philip on 19.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SlotsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
